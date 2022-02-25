aed572b941dae81e6138d5f7e08956f8c80515cd
Dasari Rajnarayana <drajnaryana@gmail.com>
2022-02-25 20:38:09 +0530

    updated code

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 import "./signup.css";
-
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import Checkbox from '@mui/material/Checkbox';
+import {Link} from "react-router-dom"
 const Signup = () => {
-  return <div>Signup</div>;
+  return <div>
+      {/* <div className="ride">
+    <p>Rent . Ride . Explore </p>
+    </div>
+    <div className="signupDiv">
+    <div className="signupbar"> 
+          <div className="signuplogin"> <Link to="/login" style={{textDecoration:"none",color:"#9e9e9e"}}>LOGIN</Link></div>
+          <div className="signupsignin">SIGN UP</div>
+         </div>
+         <div className="nameDiv">
+          
+          <input className="nameInput" type="text" placeholder="Name as per Aadhaar/Passport"></input>
+        </div>
+        <div className="emailDiv">
+          
+          <input className="emailInput" type="text" placeholder="Email"></input>
+        </div>
+        <p style={{marginLeft:"14px"}}>Mobile</p>
+        <div className="numberDiv">
+          
+          <input className="numberInput" type="text" placeholder="As per Aadhaar"></input>
+        </div>
+
+        <div className="passwordDiv">
+          
+          <input className="passwordInput" type="text" placeholder="Password"></input>
+          <VisibilityOutlinedIcon style={{fill: "grey",marginTop:"12px"}}/>
+        </div>
+
+        <div className="signupcaptcha">
+          <div className="signuprobot">
+          <Checkbox     inputProps={{ 'aria-label': 'controlled' }} />
+          <p >I'm not a robot</p>
+          </div>
+          <div className="signupcaptchalogo">
+          <img height="30px" width="30px" src="https://www.gstatic.com/recaptcha/api2/logo_48.png" alt="captcha"></img>
+          
+          <p>reCaptcha Privacy Terms</p>
+   
+        
+          </div>
+        
+        </div>
+        <button className="signup">Sign Up</button>
+  </div>; */}
+  </div>
 };
 
 export default Signup;
